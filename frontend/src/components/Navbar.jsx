2f4704ee0feefa00c91d4deaeb31758927c75d09
kkj1250 <kkj1250@gmail.com>
2024-07-24 22:41:48 +0900

    [feat] Create Navbar & Main Background Video & button - js-logic not yet
    Former-commit-id: 15b013c72e13b9e6e1707b34eed2e6a409d35109

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:740f1e0962122d48b1fcf844bbf71f0b2e0e5efc17f848864cf2025fcf916252
-size 1187
+oid sha256:1b4aa637f4dd430e4c6bcce2605593d0a207c234aee3b6d77c9221a9664022b5
+size 1518
