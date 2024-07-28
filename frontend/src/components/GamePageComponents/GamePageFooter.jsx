54b968200f9b605e58fb6040c95c5a3cd2308237
박수빈 <tns027@naver.com>
2024-07-28 15:46:31 +0900

    GamePage 수정
    Former-commit-id: d644b00f3bf5fa86d17604a5c6cb8eeae30a20c1

--- frontend/src/components/GamePageComponents/GamePageFooter.jsx
+++ frontend/src/components/GamePageComponents/GamePageFooter.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d06e52dd5e28b1b5a7540b24f5745e86434af4c786329027f56bfb005c2742e
-size 864
+oid sha256:bf265dcead21dc0a7287e8976dc47f8d1d310ba9b9eb2b565dae23e9531eb441
+size 991
