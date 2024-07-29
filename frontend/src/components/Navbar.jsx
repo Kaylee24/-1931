96ed99b163a21b0767529e64f03b15a0ec3194e6
박수빈 <tns027@naver.com>
2024-07-29 12:13:27 +0900

    [✨ feat] 네비바 수정
    Former-commit-id: 302ad5b441fd5e059cce45a02b57b0d113b232b5

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24ccf9cd5c5b2da5a4182b5d183c95bfe1893602ba670442384f84a534633756
-size 1522
+oid sha256:8bcd7e4f385f01ece464a4190d3c898d8c00de5277a2888eec4b5b0643ab7f7d
+size 1652
--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34d73e884b41a9a0f523e291e5fdac8c4d02ad97896eff50960af4b7d6573b7d
-size 748
+oid sha256:1c0d11362e485f626bd7fd34f34cc8696a61bad2d5d018756aafd10a0f483493
+size 644
