713762c9fa832a362e0a76b35975b55ce01c2285
박수빈 <tns027@naver.com>
2024-07-29 09:46:36 +0900

    [✨ feat] GamePage  개별 경로 생성
    Former-commit-id: 17f12f7eee0e78e546cd3b2955ce0ab7104994ef

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c8fbede0b7271031b938a809055c32caa96580560a64e2f0be45bb724554a39
-size 1602
+oid sha256:b6cd6c842768c9ee973ef1e2ed42c99c9818fc73685da6bcf0c31773700005f8
+size 1540
--- frontend/src/modals/FriendsListTab.jsx
+++ frontend/src/modals/FriendsListTab.jsx
@@ -1,9 +1,3 @@
-import React from "react";
-
-function FriendsListTab() {
-    return (
-        <></>
-    )
-}
-
-export default FriendsListTab;
+version https://git-lfs.github.com/spec/v1
+oid sha256:a198fbfe68155a57e393b2d8ce7d4a65a374bf24fa04bd2741cd5231d263d39d
+size 1077
