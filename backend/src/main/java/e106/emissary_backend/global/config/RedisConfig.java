ddf70a3a6f40ad1adab936b1a28138e4494b2b1d
이현규 <gusrb2776@naver.com>
2024-07-29 15:43:46 +0900

    [fix] fix redis repository
    Former-commit-id: 80e513f372b951c631d027482bdfe3fcf0a8cb9a

--- backend/src/main/java/e106/emissary_backend/global/config/RedisConfig.java
+++ backend/src/main/java/e106/emissary_backend/global/config/RedisConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f95ee7d49c9ac210f0dcbba922a2b66068590b7f9f9329fe773da4ab7ccf1e9d
-size 1481
+oid sha256:e59589e66a0f4ee18c58266f036d000307078331cce5a6486f62b244e67163b6
+size 1550
--- backend/src/test/java/e106/emissary_backend/domain/room/service/RoomServiceTest.java
+++ backend/src/test/java/e106/emissary_backend/domain/room/service/RoomServiceTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9c2e123b14d87837acf7ce67a5f04a09a550039ec4ebadb8d73e4f35c717d87
-size 3328
+oid sha256:b5455f17ccbe77175a32617c45433d9fa48ca3b13b634018430273b37f805960
+size 7316
