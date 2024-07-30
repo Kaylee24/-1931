943d99805a6bfe83a4120bef127541bade62b8ea
김송희 <kkbuh2@gmail.com>
2024-07-30 10:47:55 +0900

    [feat] FindPwModal, ChangePwModal 기본 구조 생성
    Former-commit-id: dc978b71d9295600eec2704fe99bb07754699ca6

---
+++ frontend/src/modals/ChangePwModal.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:68d96b4cc06b537774789860e625f23e1619dcf42bee2bb02ac29c3ea041d316
+size 1441
--- frontend/src/modals/SignupModal.jsx
+++ frontend/src/modals/SignupModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e52cf8cd935e8724f1cbd9e3fcfd8277a390de4f1011c24c21e2e96357f8020
-size 1963
+oid sha256:eb41c1a4f2b358fa45e5dd0dbf158d6ecb4a17e1c41bc1c5f6996f7f1f61296c
+size 1965
