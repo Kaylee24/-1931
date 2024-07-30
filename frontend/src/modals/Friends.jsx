81e2b695f50ce5f497dfbc7cc21f2c39f32aec05
김송희 <kkbuh2@gmail.com>
2024-07-30 15:58:43 +0900

    [feat] dokdo font 설치, Navbar 구조 및 관련 모달 로직 재설정 및 CSS
    Former-commit-id: 2158b7b579f3b50e0eff874cd289212343365269

--- frontend/src/modals/Friends.jsx
+++ frontend/src/modals/Friends.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76300f6ca3757938eb7c95385898b891dead74dc871c591ebaef2fd29478accd
-size 1484
+oid sha256:4724997d17e091d6f4f6ca07394170a16e5d2ca8a0b979d20d868569917de308
+size 1598
--- frontend/src/modals/LoginModal.jsx
+++ frontend/src/modals/LoginModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9476b31f7bb8686362d809a7d2856fe912d806c3505774c87c89f38b43ec3267
-size 1756
+oid sha256:94e978cb54551b270f7ad94b4c43e5651b8e38a1fefe8a2fc6de9d68a40f700b
+size 1736
