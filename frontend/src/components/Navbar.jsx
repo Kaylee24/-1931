a3c348a04281f58f4ef0880228ae830c2e8ed0a7
박수빈 <tns027@naver.com>
2024-07-30 16:14:22 +0900

    [✨feat] 업적 페이지 구조 및 호버 효과 적용 중
    Former-commit-id: 94494654aaed0efbcf3e91745b24193733c440eb

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9828ea8e3a82aa0426f3d768fecfb52cf09b153740d8a5ef6fc32ffa57d69e18
-size 2585
+oid sha256:9b53e496f199fdb56033499a657f2277316d3a574bb581ec61f3c1bee1735fcd
+size 2227
