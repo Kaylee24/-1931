a57a067664e93388d0ecc9793df1a092bb0ff23e
김송희 <kkbuh2@gmail.com>
2024-07-31 13:47:38 +0900

    [feat] GameListPage CSS 및 폰트 추가
    Former-commit-id: dfcef10d2c23316883da3b5f4d15c185955ef237

--- frontend/src/components/GameListComponents/GLFooter.jsx
+++ frontend/src/components/GameListComponents/GLFooter.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:423d62bb96d634e661444ad0763b176f3747d7f4f61147c1b77b2cb85a9b197e
-size 3955
+oid sha256:bcb20a7d8838d643095c003fe9066ca071e8e0bd0fcc5c4045c3548d7dd8ae8b
+size 4036
