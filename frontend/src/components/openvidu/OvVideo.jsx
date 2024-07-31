5cc6796d19fba205ca4a87b02c325b12e2cc969c
김강진 <kkj1250@gmail.com>
2024-07-31 17:23:37 +0900

    [feat] video component work in progress - error occured when getting tokens
    Former-commit-id: b97a0dde3563bf0c3c504d5830e901f75d33e77a [formerly c2c6912f2d6c5de066b5c6db617facf0f19ff930]
    Former-commit-id: f8195b740b265ac9dea95f1cb1b2180199f8b93e

--- frontend/src/components/openvidu/OvVideo.jsx
+++ frontend/src/components/openvidu/OvVideo.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:305dfbc37063a53e9e0dce8f92690722a56383fad0896125e8961a1155e261e8
-size 854
+oid sha256:0fa48c29c166cbdbc2fbbf925861d243f6e18b673d98e5ed1a5109f684798393
+size 663
---
+++ frontend/src/components/openvidu/UserVideoComponent.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e24d031dfca8c3315bf4a59042ac95264ba8ce62e07985a65434e81fb3298daa
+size 669
