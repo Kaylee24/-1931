67035c9802b3a73326ce253d9fad25b5ce880932
박수빈 <tns027@naver.com>
2024-08-01 12:20:56 +0900

    [✨feat]  GamePage 직업 설명 모달 CSS
    Former-commit-id: ee73d030a62bab1373f483884d1911c1d6763f07

--- frontend/src/components/GamePageComponents/GamePageFooter.jsx
+++ frontend/src/components/GamePageComponents/GamePageFooter.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf265dcead21dc0a7287e8976dc47f8d1d310ba9b9eb2b565dae23e9531eb441
-size 991
+oid sha256:3307159915166af1d62f67dc2562d4e1def4c6eb514233a1b02cc315298a1cb8
+size 1214
