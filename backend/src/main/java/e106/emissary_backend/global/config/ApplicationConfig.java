f88a7b27387e9da19d498c18baa6c5c27e033818
김강진 <kkj1250@gmail.com>
2024-08-01 11:56:40 +0900

    [feat] update cors policy - allow  origin for test
    Former-commit-id: 861af336f85dbec61790cbc73abf58ab7907ced8

--- backend/src/main/java/e106/emissary_backend/global/config/ApplicationConfig.java
+++ backend/src/main/java/e106/emissary_backend/global/config/ApplicationConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eae559134dec8fabe25405a9a141a590f7198e1ce65bb99d740ab8a95c09e0fc
-size 2335
+oid sha256:7d67d35469f6e11b9aa79befe33cb3748f8fabf04f85f00234b882f8b25f5c8f
+size 2360
