8a7fb2e5c044f4395ab3086fc86fa4e5a67ba9fb
김강진 <kkj1250@gmail.com>
2024-08-01 13:41:34 +0900

    [fix] cors error
    Former-commit-id: 077f9c8e4d1d28d51b7832f143ac0bf2e005fb0c [formerly 10baf189473050c92fb3ba5f7fc5301ca89a0b2f]
    Former-commit-id: 17e4a24f40ec0b835bb858bfdd8b8526f3e2b62d

--- backend/src/main/java/e106/emissary_backend/global/config/ApplicationConfig.java
+++ backend/src/main/java/e106/emissary_backend/global/config/ApplicationConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d67d35469f6e11b9aa79befe33cb3748f8fabf04f85f00234b882f8b25f5c8f
-size 2360
+oid sha256:a8986cf283067f609128b22e2737cf7da55118cca46fac11a35c88401a108c3b
+size 2409
