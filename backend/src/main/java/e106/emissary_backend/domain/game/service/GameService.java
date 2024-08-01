c6c46250999bc4a7a189d0f44dd9b62bf1e761d8
이현규 <gusrb2776@naver.com>
2024-08-01 14:12:12 +0900

    [feat] implements remove user
    Former-commit-id: de72f35072fa7ee40e9ca8a24489f7667b7cf6a0 [formerly 24edf071f7ec84e9f45658dfd3d4a0a3fc3e822c]
    Former-commit-id: 49df77c267cc6c06da099771419f29f460684a5d

--- backend/src/main/java/e106/emissary_backend/domain/game/service/GameService.java
+++ backend/src/main/java/e106/emissary_backend/domain/game/service/GameService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f86a9008a3ee55f51d3f90f7d665b4a9680efde0a76034e43a6875ab0d754c9
-size 8061
+oid sha256:f72284551527dfa26414a9c836a4c946446e6f8d6fe1b12642b47d8367892f9d
+size 8648
