2fe328a488c4a833b45679e860189b1604e96125
이현규 <gusrb2776@naver.com>
2024-08-02 03:14:32 +0900

    [fix] fix security cors and authorization
    Former-commit-id: 937a067c4e56ad7ee6d9034ecd7aa4d7dfd189b8 [formerly 8cec00a1afc298d3070a6dd089ea2f829663afdc]
    Former-commit-id: 1e8df28731b8f3a8d4d942742d9f14e0442e3aea

--- backend/src/main/java/e106/emissary_backend/global/config/SecurityConfig.java
+++ backend/src/main/java/e106/emissary_backend/global/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:928bbe08d0075ecbdee4b918c918290df30828956012a8101917ae92249542ca
-size 6648
+oid sha256:c7ff4530afdae47a117ae957da64a24931f86311b66f292ddb42c6f49925f853
+size 6682
