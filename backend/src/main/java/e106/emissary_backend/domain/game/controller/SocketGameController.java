982beb98610402bcbaf596157fcf3e0dda78de54
이현규 <gusrb2776@naver.com>
2024-08-01 17:13:50 +0900

    [feat] already remove user error
    Former-commit-id: c560cab35606c7251d2a2fa6036ec84b9b4315e3 [formerly 217b3246bf8a2b2bc82bfb075c0f8447e637409b]
    Former-commit-id: dce3807e637a592ab6a2a54cd05b15da70849dda

--- backend/src/main/java/e106/emissary_backend/domain/game/controller/SocketGameController.java
+++ backend/src/main/java/e106/emissary_backend/domain/game/controller/SocketGameController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80b4ac43d8458b030f4ca291eade6997bf3009553401590c5acfc85c1b52b58a
-size 2960
+oid sha256:7f93019588ae0de46c8450545f95b58f1f43b0ab846c708960a55e01b087c0f6
+size 3003
