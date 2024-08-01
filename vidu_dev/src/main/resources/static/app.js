c969d8b507de4fdaa82c20401c5ecec72efa538d
김종훈 <boll0918@naver.com>
2024-08-01 14:26:00 +0900

    🐛fix(#647): Resolve OpenVidu session reconnection issue
    - Initialize streamManagers array when user leaves the session
    
    This fix resolves the issue where users encountered problems when
    rejoining a session after leaving.
    
    Closes: #647
    
    Former-commit-id: 5603434a0e61cf024b4956bc2f0907a20764de9f [formerly 8e5e5805ff31a10cd530eeaebb44ea74dc7b758b]
    Former-commit-id: 061a925be1ce14f0f93011fb0af6c70d715604d3

--- vidu_dev/src/main/resources/static/app.js
+++ vidu_dev/src/main/resources/static/app.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:812031ec3c4641983dfbbdfba2764d111e96357e7ba26a942cecb312f2047f5e
-size 4239
+oid sha256:b8ca304cb9777e2de0d6dcfc2c067fc91215546b2233d684fa0577f936cec8d5
+size 4297
