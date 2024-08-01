fb9fe4af1208937da0128fca99daef7331c43d8d
이현규 <gusrb2776@naver.com>
2024-08-02 00:40:08 +0900

    [feat] implement appease person
    Former-commit-id: 3ea977146bc034339e9157aa80e445ac37916c79

--- backend/src/main/java/e106/emissary_backend/domain/game/service/publisher/RedisPublisher.java
+++ backend/src/main/java/e106/emissary_backend/domain/game/service/publisher/RedisPublisher.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b03b0751f50de6ecc5407059afff5dc4c19369a79ea0b2127ca3f8c83e03a1f
-size 1318
+oid sha256:d56eb41522a5a3b86646293154d45082821df50ac9d9a04626317299a1a23ae1
+size 1466
