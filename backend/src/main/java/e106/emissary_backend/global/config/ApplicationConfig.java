3c5cf7c10aae03d857f3780cd34e0bc4f298d29c
김강진 <kkj1250@gmail.com>
2024-08-01 16:15:34 +0900

    [fix] cors error fix - config security cors setting
    Former-commit-id: 40c54c88f6f1736dbe52cb75766bb8cd4c66e528 [formerly 7b990e361f28b416ac9ecb95ddb4ef3a8135254d]
    Former-commit-id: c1c00133be225b6764fda41f518dae33ee71d0a8

--- backend/src/main/java/e106/emissary_backend/global/config/ApplicationConfig.java
+++ backend/src/main/java/e106/emissary_backend/global/config/ApplicationConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8986cf283067f609128b22e2737cf7da55118cca46fac11a35c88401a108c3b
-size 2409
+oid sha256:0e1079c03d2d092d5293c75ee3a469369ffb3b51051ab4e73c5fea5a39cae357
+size 1842
