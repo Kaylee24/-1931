a63827b4867294be9a4dec6b2e711f031092155f
이현규 <gusrb2776@naver.com>
2024-08-01 17:20:46 +0900

    [feat] emissary kill error implements
    Former-commit-id: 26b4eca911ee99ef9a19ca67f5778dc773f50303 [formerly eccf94c1affc9c361ea94e20918bab6ac5744241]
    Former-commit-id: bd4029f234d7fad60ddabc85fc0dce8edc6ca082

--- backend/src/main/java/e106/emissary_backend/domain/game/service/GameService.java
+++ backend/src/main/java/e106/emissary_backend/domain/game/service/GameService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3253058b722a0682fb42d349be7d806ae026be60c3a3769fd48da713bf9605a
-size 9701
+oid sha256:22dbad53ff958dabed05d011d7a7bb735e745fc3312545ed53e1bff4ebb232ad
+size 10150
--- backend/src/main/java/e106/emissary_backend/global/error/CommonErrorCode.java
+++ backend/src/main/java/e106/emissary_backend/global/error/CommonErrorCode.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab27260ec69f2d7386930c5f004a968a53f030cf74cebcabce0ce71a2b96dcf9
-size 915
+oid sha256:ae85c2c71a9a2dce6617a48f8cb8c2dd8db53496871f53d2192509f79aa5b09a
+size 998
