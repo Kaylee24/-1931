ff0a57aac8ef11b85e58923696702e5320915302
박수빈 <tns027@naver.com>
2024-08-01 10:53:58 +0900

    [✨feat] GamePage (방장)게임 설정 모달 CSS
    Former-commit-id: d04bcce8ae25780eddf37eb904aa4d6fd43b51e1 [formerly 34de2fcccfed74ec19fb5831fc1949b96b358b00]
    Former-commit-id: 60924beaf4569a3c71b67247f366b1a71a411e9c

--- frontend/src/components/GamePageComponents/System.jsx
+++ frontend/src/components/GamePageComponents/System.jsx
@@ -1,13 +1,3 @@
-import React from "react";
-
-function System() {
-    const alarms = ['밤이 되었습니다', '낮이 되었습니다', '밀정이 활동 중입니다.', '첩보원이 활동 중입니다.', 
-        '토론을 진행하겠습니다. 토론을 진행하는 동안 밀정으로 의심되는 사람을 투표해주세요.', '최종 반론 중입니다.']
-    return(
-        <>
-            <p>{alarms[2]}</p>
-        </>
-    )
-}
-
-export default System;
+version https://git-lfs.github.com/spec/v1
+oid sha256:f7f03cb206b894139d2b2fc7d21d2bd1ab8441c8a9557c7cd3f0d65851cb160c
+size 524
