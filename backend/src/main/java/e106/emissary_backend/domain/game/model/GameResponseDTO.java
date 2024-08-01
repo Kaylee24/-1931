74db070b77e48089428ac3ef2aa3fe924f184208
김현우 <hwk216@naver.com>
2024-08-01 09:30:24 +0900

    [feat]url 변경
    Former-commit-id: e6d64473bfeeff51ca1c399985938b1df739d662

--- backend/src/main/java/e106/emissary_backend/domain/game/model/GameResponseDTO.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:065d222f8ab19d72bdd15f7b0b6136a802924df4f4a09852e3e45aebe6425b6e
-size 1488
--- backend/src/main/java/e106/emissary_backend/domain/user/service/UserService.java
+++ backend/src/main/java/e106/emissary_backend/domain/user/service/UserService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66424b42c9abc30c53d6cae3d13252f675a5b39982834ab2feae618639c9ba37
-size 6658
+oid sha256:8714fa1d95934b60b59c3b872142d35a1c34f514f563ac1e8981de9e33ff94b6
+size 6914
