24662f44575e8936a24e33b037acc78e9f875d40
김종훈 <boll0918@naver.com>
2024-08-02 11:21:11 +0900

    WIP: feat(#657): Implement day/night cycle with video/audio control
    - Add signal sending for day/night changes
    - Implement client-side handling of day/night signals
    - Control all users' video and audio based on day/night cycle
      - Turn off video and audio for all users at night
      - Turn on video and audio for all users during day
    
    TODO:
    - Create new session for specific users during night phase
    - Implement communication in the new night session
    
    Partial implementation of #657
    
    
    Former-commit-id: 1b1496659398baaec96df3bb2d410a42a63bfe2e

--- vidu_dev/src/main/resources/static/connectionHandler.js
+++ vidu_dev/src/main/resources/static/connectionHandler.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb086e5837b58d5f2514e197eaa70f11bcec83deb99d158cc2c272f6f4c8cd75
-size 4568
+oid sha256:56f4133f209d2c5c0d2d24e92cb3929f7dc53c998d0b55ecf491e5302aa2b527
+size 5583
