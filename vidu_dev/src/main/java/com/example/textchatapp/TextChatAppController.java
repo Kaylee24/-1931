64acd35dbf51780bfc7e1595d2116753ae9c9346
김종훈 <boll0918@naver.com>
2024-08-02 11:21:11 +0900

    WIP: feat(#657): Implement day/night cycle with video/audio control
    - Add signal sending for day/night changes
    - Implement client-side handling of day/night signals
    - Control all users' video and audio based on day/night cycle
      - Turn off video and audio for all users at night
      - Turn on video and audio for all users during day
    
    TODO:
    - Create new session for specific users during night phase
    - Implement communication in the new night session
    
    Partial implementation of #657
    
    
    Former-commit-id: 886411146d9df5693e2c0fc37cfb83078f87f67b [formerly d4acd116a3b2ab6ad7292be605f99a4ce7c78325]
    Former-commit-id: 7665d06bf0e3eb2534702736eab2035878d14b67

--- vidu_dev/src/main/java/com/example/textchatapp/TextChatAppController.java
+++ vidu_dev/src/main/java/com/example/textchatapp/TextChatAppController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afcba92ce7a0b31b30b376212f20f9f37538e2b0f88ffb55c4e9bee73d915de8
-size 7617
+oid sha256:57ea8afe3ebcb169e4076bb73c7b6a5a4e79617e95c596eefd411a85d567d7e4
+size 10427
--- vidu_dev/src/main/resources/static/app.js
+++ vidu_dev/src/main/resources/static/app.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8ca304cb9777e2de0d6dcfc2c067fc91215546b2233d684fa0577f936cec8d5
-size 4297
+oid sha256:425b065cd427f9633d8d05d77847ddbf43b8aa09ec378b6b21af4c57f4f78cd3
+size 5329
