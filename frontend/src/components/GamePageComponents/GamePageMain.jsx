1bcd445a8459a8f1d3a883477c131a7761b18d6d
kkj1250 <kkj1250@gmail.com>
2024-08-03 23:04:52 +0900

    [feat] can see other people, but already exist user can't see bug has reamined. leave session not worked
    Former-commit-id: 47e76c39582beafa5af40925d536f77e0a2f13d2 [formerly 14b314977ee607e3fe671741ecadf301331d034c]
    Former-commit-id: 366bc3d32f2fec3c651fc6d1fbf7e32e4276c9a1

--- frontend/src/components/GamePageComponents/GamePageMain.jsx
+++ frontend/src/components/GamePageComponents/GamePageMain.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e29d4b6542c054db9476d80c6135a04a22a55a7509fdabbe57115450cd64016
-size 4319
+oid sha256:403391cd339444d56582a0c7d28b61b62a26ab24cc46d77d70d26af8973e14cc
+size 4331
