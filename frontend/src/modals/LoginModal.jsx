8708ea20370b5793b12b97b2da384965c1818e8b
kkj1250 <kkj1250@gmail.com>
2024-08-04 16:55:15 +0900

    [fix] test_fe
    Former-commit-id: cb69267f452e21fc4f2d8fb71e0ff15b969f8a5d

--- frontend/src/modals/LoginModal.jsx
+++ frontend/src/modals/LoginModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:183428fcd91bbc987cff769375969987543e8f86a53a75102fad34f2fe193f96
-size 3406
+oid sha256:f58a939611baeacd0f1f5dd6fe9d5f3ad038f5018c3544faf494f3357e1b3ea0
+size 3409
