b5e3822228bb4f817ccbded7cdafc79f88986193
kkj1250 <kkj1250@gmail.com>
2024-08-04 12:36:53 +0900

    [feat] Session Exit job done
    Former-commit-id: 5b2a820020bd382f27200814417d8c298952567a [formerly 9641a25a07d6c40fc6427c4c587e8a5c8cbdc93c]
    Former-commit-id: f835a4e1e14b5302759c46068b9dafd0cc078247

--- frontend/src/components/GamePageComponents/GamePageHeader.jsx
+++ frontend/src/components/GamePageComponents/GamePageHeader.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:09d9cf3ab44b3bb9f15e29165e96f76b34c7441d4110712999622287cd7fb093
-size 4026
+oid sha256:d9e6143bed66950e698d7840a6c48f50c357d4a4e7021cbebc6eca8f1dadce0c
+size 2486
--- frontend/src/modals/CreateRoomModal.jsx
+++ frontend/src/modals/CreateRoomModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d2f1554e6186a02edea9c6dfd3f6188309a446f330804813c4dc6670fef9f24
-size 3857
+oid sha256:e3213dded5a68eb0f734bbb6618764a4049f7fd811bb75bca423acb5be33d5a9
+size 3793
