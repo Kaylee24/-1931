ae82a67f1c88672fc9f2357ce51372d9078d37a3
kkj1250 <kkj1250@gmail.com>
2024-08-04 16:02:15 +0900

    [fix] login error - ([fe] formData -> json)
    Former-commit-id: d2ce8faa6b63c16c589ff03fddb93dd3e7c20036 [formerly 518e9be4872679dfba2bd6d383265b6b50ec5658]
    Former-commit-id: ada55ba95d6b8b7df0078013195842f08230fe59

--- frontend/src/modals/LoginModal.jsx
+++ frontend/src/modals/LoginModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66c4992deb010f2c32dd692396288a9dc0a2403aee96c70794300720adde3dfb
-size 3428
+oid sha256:321c5497de931ffe1f179d6a55d52dcc89cc4f5516c37090746643c935cb7187
+size 3406
