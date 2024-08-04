3d127268e8e262c9d1e7975c30030c0442cafeda
kkj1250 <kkj1250@gmail.com>
2024-08-04 17:42:16 +0900

    [fix] try to resolve mixed content
    Former-commit-id: 3f544d6298c1140665122f5fe28fdb8019d85eed [formerly 884edb1f8d049389a97fbe1f757a93a1575db6ea]
    Former-commit-id: d76fdd2581edecd730b42048bc24ff097e06bacd

--- frontend/src/modals/LoginModal.jsx
+++ frontend/src/modals/LoginModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f58a939611baeacd0f1f5dd6fe9d5f3ad038f5018c3544faf494f3357e1b3ea0
-size 3409
+oid sha256:810ed8537e4911c68f744f0aa75e927f5d04407763bb47addf5f49a849a8c522
+size 3405
