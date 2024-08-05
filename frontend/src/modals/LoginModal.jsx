477bcb87bb6ff2c762420fa54f6acfc18acb6126
김강진 <kkj1250@gmail.com>
2024-08-05 13:59:57 +0900

    [fix] merge Conflict error fix
    Former-commit-id: 96fd9fd3eff7b4b861cf370798b96732f0793463

--- frontend/src/modals/LoginModal.jsx
+++ frontend/src/modals/LoginModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2d75451d4ca2a34365e94777da6011ee574ed985bb7563ab633f26812ffaf3c
-size 4678
+oid sha256:4b0af32312fe48c372dae9df3f922b5f7547f2e02cdcaf6c81faa8cf329fb0bc
+size 4585
