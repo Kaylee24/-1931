bcd42268c29925189dd2db83e5b9150c59d2b26e
kkj1250 <kkj1250@gmail.com>
2024-08-05 21:50:22 +0900

    [fix] reissue error - cookie -> request body
    Former-commit-id: 9b3ef530e82456c2db9690ebaf6280a2e1454796

--- backend/src/main/java/e106/emissary_backend/domain/security/Controller/ReIssueController.java
+++ backend/src/main/java/e106/emissary_backend/domain/security/Controller/ReIssueController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:462a4084e340b2b015215bccf1ffd63047cba688ae23075be16db5e6a0fc0db5
-size 5226
+oid sha256:a61501111f5df82a0e9f5ead813caf514f7b5c0b889ed6fee6bdd63850fb7d6a
+size 5419
