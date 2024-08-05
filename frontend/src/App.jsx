070fa9cc70103b62cfcf843e2ca1a2aa2a65cffd
김송희 <kkbuh2@gmail.com>
2024-08-05 21:18:59 +0900

    [fix] reissue token 전달 방식 Authorization 등으로 변경
    Former-commit-id: c7a4bead75c437d91119cc358bee2a2ca2626862 [formerly ae257cc3feffa792945a0400e7d63a7ad001b5cb]
    Former-commit-id: e7673ea1ded982f3ad7bb8b6d4959aef06c34c1c

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5eea1c7ebe484fddc327c4c249dcc60b0ff2c3c00059ef4ac26e99881bb91b71
-size 3365
+oid sha256:0b48e48f82fc6ee3d20c71167ed661b7033f4446a178009ba6d43217854af6ed
+size 4372
