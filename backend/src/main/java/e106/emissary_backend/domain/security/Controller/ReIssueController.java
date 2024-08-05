12a2e3c4a56b6af6075948bd61df0904ec5db8c8
김강진 <kkj1250@gmail.com>
2024-08-05 15:53:17 +0900

    [chore] add fail status for debugging
    Former-commit-id: f19ef15f90b9151ed51a35cc8ea9f7700955e59b

--- backend/src/main/java/e106/emissary_backend/domain/security/Controller/ReIssueController.java
+++ backend/src/main/java/e106/emissary_backend/domain/security/Controller/ReIssueController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba91de1b78e7431595547efd41bf1dd9ae28f5fe818b35e88c4972d38fcfd720
-size 4915
+oid sha256:2480c6b1c9f24fab77d9a59afdfb9c3d718b2f930954dbfd97b7d7c9d9e6ac3a
+size 5179
