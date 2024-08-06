e153554d36e372b0ec5ed3390695d8034b2a3800
박수빈 <tns027@naver.com>
2024-08-07 00:20:45 +0900

    [[✨feat]  Game Page 방제목 및 (방장) 게임 설정 API & 준비/시작 버튼 웹소켓 연결 코드 작성
    Former-commit-id: cef5413fed5d93a27f216c0472b5e675555d28a5 [formerly 678e281fd7bb930e8bdf85133d71ad19dbd23d34]
    Former-commit-id: f5f0fcc2a87494b2c80be8fda52926dc7bea1665

--- frontend/src/components/GamePageComponents/GameReadyStartBtn.jsx
+++ frontend/src/components/GamePageComponents/GameReadyStartBtn.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8039889cfe7ec07a7a4b7cdd57618cac7fc9c61297515659b1adbc7d7dd51805
-size 3556
+oid sha256:2c6b6f900b5b212e85d10fced80976c8ae56acd723aa1fd710b1cd8bf699e1cf
+size 5607
