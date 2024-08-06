bffb17d131117a106a991c0e2995e888a2b2ef2b
김송희 <kkbuh2@gmail.com>
2024-08-06 12:31:45 +0900

    [feat] Logout 시 Navbar.jsx, App.jsx 즉시 반영
    Former-commit-id: f748ed61051ddf32c064964d000f27d5acc09ddc

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a5765d860d1c1598561797ebf224290d8f8a81e090b934941a62747b0bfdff2
-size 3475
+oid sha256:e96ca6a4edff1d84c171476ee37e0318167c612e5fe5a7d8ba3c7c35c79b52bf
+size 3482
