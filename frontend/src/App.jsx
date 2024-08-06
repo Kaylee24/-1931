d5c69f80406bf48aa33d7faacf74bbcd27fcc47b
김송희 <kkbuh2@gmail.com>
2024-08-06 10:55:47 +0900

    [feat] SignUp API 연결중 - 403 Error
    Former-commit-id: b67603a2e1a1121ac0f7606a6a88d771c3c4cd64

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1bc6263350a4221fe71955ed145f7f479d337d8c13ffa1e33fdfc95d96cfcd6e
-size 3471
+oid sha256:6a5765d860d1c1598561797ebf224290d8f8a81e090b934941a62747b0bfdff2
+size 3475
--- frontend/src/modals/SignUpModal.jsx
+++ frontend/src/modals/SignUpModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb41c1a4f2b358fa45e5dd0dbf158d6ecb4a17e1c41bc1c5f6996f7f1f61296c
-size 1965
+oid sha256:3d13c22f7e826528c7cc50192101d3c81407d0c690112f34ff4155b13517c693
+size 4525
