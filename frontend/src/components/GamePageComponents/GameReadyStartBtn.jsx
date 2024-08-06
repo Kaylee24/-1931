4f2633522f2a9725ca2fce1f93806380c27df386
박수빈 <tns027@naver.com>
2024-08-07 01:22:57 +0900

    [✨feat] 직업 모달 웹소켓 연결 코드 작성
    Former-commit-id: fa15c95d30fb62b37d7718729f166db80bfea9d9

--- frontend/src/components/GamePageComponents/GameReadyStartBtn.jsx
+++ frontend/src/components/GamePageComponents/GameReadyStartBtn.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c6b6f900b5b212e85d10fced80976c8ae56acd723aa1fd710b1cd8bf699e1cf
-size 5607
+oid sha256:b5b09a280a22075cfc7fabf5b685fa6569b48c85f010668e07dab850426817a5
+size 5705
