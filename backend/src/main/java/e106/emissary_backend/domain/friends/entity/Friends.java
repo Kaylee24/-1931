0081543b68bb723f3280e83c4b3164675b8712a3
김현우 <hwk216@naver.com>
2024-08-06 12:39:30 +0900

    [feat]get 요청 파라미터로 변경 및 친구 개발완
    Former-commit-id: 1af71ddd820a02b57a4cbd8eb98aa373eb64483a [formerly e03f0dbc840f4989b37c6042b71bbc5675c718de]
    Former-commit-id: 3098a67c31d59915d73de681c9597ab6f712cede

--- backend/src/main/java/e106/emissary_backend/domain/friends/entity/Friends.java
+++ backend/src/main/java/e106/emissary_backend/domain/friends/entity/Friends.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ef22e431dda127e686c970a103cfa24ec3ab23709b7402dd6205ec94b485445
-size 924
+oid sha256:acf75e66da40af12b03fae54732ca810eb04ea48ef24b65c9fd5d61fc616c479
+size 1074
