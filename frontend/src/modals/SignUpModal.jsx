ef5fb6a01404f7ac44b07389ad825797dd387964
김송희 <kkbuh2@gmail.com>
2024-08-07 11:15:48 +0900

    [refactor] SignUpModal handle 함수 email/nickname 분리(/api/mail 사전작업)
    Former-commit-id: b1ff0114e6821f40760a6610642a2295edb6b21f

--- frontend/src/modals/SignUpModal.jsx
+++ frontend/src/modals/SignUpModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ef9be3ee0cc7ec9531fbbe8f384daa4156d785b74d42900cb5c358cc8a29d6a
-size 8483
+oid sha256:1775fbf23cf8714633401010a6ebc08e673b2cb882365cd009af60e59da24bf2
+size 9554
