32439af288e17ceccd94d07a743cf33a24cfd49c
김송희 <kkbuh2@gmail.com>
2024-08-07 23:14:57 +0900

    [feat] GLMain API 연결 - 기본 코드 작업 중
    Former-commit-id: 95d146676d3fff61f7d0dc967584e30547ba4ff3 [formerly 5caa43ac4b32601b3504e5595f5710769ab05448]
    Former-commit-id: 55686a4287005c3589c7d51ec2f211e85cae8262

--- frontend/src/components/GameListComponents/GLMain.jsx
+++ frontend/src/components/GameListComponents/GLMain.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e00cece11cef7faadaec6b5b06bd1cc3720a34f559888935dc3531a4c7188ff1
-size 3654
+oid sha256:9e2461e9c174294072037e849c2ca264640784b3d2de9dfa137194c998eae521
+size 2190
