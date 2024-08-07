85e48f6edf4842db9866096da3f2ba310b118154
박수빈 <tns027@naver.com>
2024-08-07 17:13:50 +0900

    [✨feat] 게임 페이지 에 구독하고 방 아이디 가져옴
    Former-commit-id: 4acc764a279a8553dd8cf55f61d19306572698cb

--- frontend/src/components/GamePageComponents/GamePageMain.jsx
+++ frontend/src/components/GamePageComponents/GamePageMain.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd7b0f681cde52497ab1cc83052931bcfc0a31d727588c37b48bb0067660d045
-size 13878
+oid sha256:f485d097812822f2dba5eb7aa5c98b083bcd304c0d17074ced559245070b2ed8
+size 11823
