d036c0d0f1c088b59e7340d9a6224e1045394036
박수빈 <tns027@naver.com>
2024-08-07 16:44:27 +0900

    [[✨feat] 레디버튼에서 게임메인으로 props 보내기
    Former-commit-id: d83e567e0c666744eb9810d5d50670f62abbe9db [formerly 7cc48092f536ab06779e2f6fd6b401a98aa1119e]
    Former-commit-id: eb1f25afabc171a6267b2ad860bfc0d744189b40

--- frontend/src/components/GamePageComponents/Monitor.jsx
+++ frontend/src/components/GamePageComponents/Monitor.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d67e96021d4e4f6c7d91089a7e4972852123f498112dabce3e2edd27ac9f095
-size 2332
+oid sha256:4aed58d1c80987cd1af485dc6bb7d35628e18851a492c2c03b98a5b94a05e098
+size 2346
