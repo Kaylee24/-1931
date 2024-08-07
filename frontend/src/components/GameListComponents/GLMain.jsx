3a14a45c7cd2dd7c5ec312150be8ba3afad879a9
김송희 <kkbuh2@gmail.com>
2024-08-07 23:23:19 +0900

    [feat] GameListPage API - GLMain 방 목록 받아오기
    Former-commit-id: 14ac018aafbf01b38df6806a64f2e2b605ad7c2f

--- frontend/src/components/GameListComponents/GLMain.jsx
+++ frontend/src/components/GameListComponents/GLMain.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e2461e9c174294072037e849c2ca264640784b3d2de9dfa137194c998eae521
-size 2190
+oid sha256:bc01cbe2624496fe1ce7a7ff143aa34ed7e53e9fac72cdd63349d7ce9432aa77
+size 1418
