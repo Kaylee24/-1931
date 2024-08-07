18ac169ad36ce84c87e043d593e2739bcaeffdae
이현규 <gusrb2776@naver.com>
2024-08-08 03:19:03 +0900

    [fix] implements game result save in db
    Former-commit-id: 8b8c8740d466becc2d9aa634b55d44d6f1320d1c

--- backend/src/main/java/e106/emissary_backend/domain/game/service/GameService.java
+++ backend/src/main/java/e106/emissary_backend/domain/game/service/GameService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68aa1442ace4cd9a04afed8afef30966fee148ea58e4605280401b9f75e0cfad
-size 15032
+oid sha256:83b48b91e08e88240d79a4356afdba18c439bd2fa2691c436fb61e31b9017a38
+size 15171
--- backend/src/main/java/e106/emissary_backend/domain/game/service/timer/task/EndConfirmTask.java
+++ backend/src/main/java/e106/emissary_backend/domain/game/service/timer/task/EndConfirmTask.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c104d3cf449438c2bb8183cb74e28460c52b445f78cd00c6f22617af3322ce8
-size 3696
+oid sha256:2ae3d09686d5505dfd451ef7f50e3ad5b03c306dc649ab8e31e4bd9ca41280ac
+size 3617
