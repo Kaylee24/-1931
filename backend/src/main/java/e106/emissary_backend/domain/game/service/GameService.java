97a3da2519ebc49667c4fac15be1ef50ff9b96f9
이현규 <gusrb2776@naver.com>
2024-08-08 08:55:59 +0900

    [feat] add isMe in gameSetMessage
    Former-commit-id: 67b3f35f1688d496149b82382930fdf0e0dcd968

--- backend/src/main/java/e106/emissary_backend/domain/game/service/GameService.java
+++ backend/src/main/java/e106/emissary_backend/domain/game/service/GameService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cef88af2cc6f2b49d009f21b54d87d3ccced800150c06a1fa865e25e5158008c
-size 14909
+oid sha256:abd381d20b560d7e07bbd6def12609dad55aae6cd638b6da6e4dc319481f1aca
+size 14986
