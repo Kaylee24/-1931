14a896dd051ce197debaea90b67a626fd8b15b06
김강진 <kkj1250@gmail.com>
2024-08-08 21:32:50 +0900

    [feat] send Room Id when Create room to navigate gameroom
    Former-commit-id: d6461db8fb5124410cc06d0de327afcff93ded50 [formerly a1a771be5a0d681cff4b03af704a24894f264611]
    Former-commit-id: 4f9eff961b894e0d04535b1def7143e20b4828cb

--- backend/src/main/java/e106/emissary_backend/domain/room/service/RoomService.java
+++ backend/src/main/java/e106/emissary_backend/domain/room/service/RoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7c3b0cd12b59cd4a00e4bce0262fccabc55bb094daded16544dddd64c2867d5
-size 17526
+oid sha256:bde6d3e65487f2096d21d35b5d4361aa703bf9d1765655e8e47966d7ecb2588e
+size 17549
