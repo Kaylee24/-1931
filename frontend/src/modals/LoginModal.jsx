b77ababf90daac44c1c5b1818f3be131b3e8b8f7
김송희 <kkbuh2@gmail.com>
2024-08-08 16:11:27 +0900

    [feat] SignUpModal axios 요청 버튼 추가
    Former-commit-id: 75b6bd5c2e2de0c525d1e4d19d8e7e5fdfcc8a52

--- frontend/src/modals/LoginModal.jsx
+++ frontend/src/modals/LoginModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9cf6b480054cb6e5145bf7a4312e2eb498424ee7afcbb6c6545edac1268823d8
-size 4131
+oid sha256:2e97d71533868b82521b121afdbb9699ac583ac772e84406d3e982df47a7baff
+size 4102
