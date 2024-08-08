0c773ccec094f6f71d7677471e11e3762fe65c02
김강진 <kkj1250@gmail.com>
2024-08-08 10:45:56 +0900

    [feat] add room ID to GameList & room Detail controller added
    Former-commit-id: f335f308595a4db70e69190ca0c4206e2e2cd1ce [formerly 94ce1cfd3aa3e34e224c276b4d4d6a1f44c9143d]
    Former-commit-id: e64252b5c79a0260fb86384c37f78b970843a0cd

--- backend/src/main/java/e106/emissary_backend/domain/room/service/RoomService.java
+++ backend/src/main/java/e106/emissary_backend/domain/room/service/RoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7b52124692edb8ae178c405298fc629ab6b7742c40ff3db5f7d44adca7d3b4f
-size 16677
+oid sha256:d7c3b0cd12b59cd4a00e4bce0262fccabc55bb094daded16544dddd64c2867d5
+size 17526
