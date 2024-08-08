30edfcb2a36cf82f08e724a1674416629847aa1b
김강진 <kkj1250@gmail.com>
2024-08-08 23:46:41 +0900

    [feat] devide achievement controller
    Former-commit-id: 040a853b6430315922f27547446ef845915f1334 [formerly 460ff8fb2678fb357d29411d1c4ec4f657149990]
    Former-commit-id: 251818c1066b433486e7c3bd3154db2866bc1e57

--- backend/src/main/java/e106/emissary_backend/domain/security/config/JWTFilter.java
+++ backend/src/main/java/e106/emissary_backend/domain/security/config/JWTFilter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d48339377d508ec6f7a8cc40fcec511820013a8794ede51870bdcd2830a24b01
-size 3967
+oid sha256:21612a5aadab755666e0a8d06254adec820d298b42ffb8b5015a14d2af00c56e
+size 3569
--- backend/src/main/java/e106/emissary_backend/domain/user/controller/UserController.java
+++ backend/src/main/java/e106/emissary_backend/domain/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8918b462994f2a0116f69870561733e30dc288a805befbd5dd8403e6776c3a4a
-size 5084
+oid sha256:ff58bf9379d865e634f5eeabcaca9711f051f03b3787e51fd98d2839b4612bf8
+size 5069
