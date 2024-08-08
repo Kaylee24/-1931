bb0a8b1dc353cc81048cab285d2f540bd6e8d0dc
김강진 <kkj1250@gmail.com>
2024-08-08 21:34:39 +0900

    [fix] missing ;
    Former-commit-id: 461fd6a3f8d14c6e314e2d5d943a07840f03b7c9

--- backend/src/main/java/e106/emissary_backend/global/config/WebSocketConfig.java
+++ backend/src/main/java/e106/emissary_backend/global/config/WebSocketConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a395cbab670200f0e8ad823978375c9cfd37c17cfeed8353ca06493d044f01b
-size 1447
+oid sha256:c7cdf935a7b36e02a597df39a5a66fe530f592cb69a4779d7b31861a89e1a6bf
+size 1448
