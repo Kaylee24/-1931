5488aa0da3eae66592cfd44e52b3d74974ff904d
김종훈 <boll0918@naver.com>
2024-08-08 12:31:00 +0900

    [fix] leaveSession, getOutPlayer 함수 수정
    Former-commit-id: 391bd95db780fdd8e1f291398ce9b8f3b867a62f [formerly c970953b211ee6dcc19cbd5bd63409add6a5b416]
    Former-commit-id: 90a38a5b748f0c9886825232c63512b02411633b

--- frontend/src/components/GamePageComponents/Monitor.jsx
+++ frontend/src/components/GamePageComponents/Monitor.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d64a860d0a16414598873f9113535e611b8cfee37374c21c51a3a4fdfebe3e4
-size 3683
+oid sha256:32a4ad8a2ce3c57108be2b8f394d6a27e10ed12d1667c2961a7ec51b7ae1cda1
+size 3705
