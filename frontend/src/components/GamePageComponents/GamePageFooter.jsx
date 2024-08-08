0d3fdddd6bb8da4795004f7afb308a3e83042f79
김종훈 <boll0918@naver.com>
2024-08-08 16:43:44 +0900

    [feat] 밤/낮에 따른 채팅 제한 기능 구현
    Former-commit-id: 0e3904ac38ad6ff2dd89324256e0f03e59be9cc7 [formerly 1e86acee62ad7e4b04333bb16130ed0b5a4a79eb]
    Former-commit-id: 1371ef2316d17f9c2b412121482d7882d4a2d123

--- frontend/src/components/GamePageComponents/GamePageFooter.jsx
+++ frontend/src/components/GamePageComponents/GamePageFooter.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d574520cca55491948d09af1a53fd36f58ee836ef630cd7f6b001daee1cc29ec
-size 1353
+oid sha256:715da1225c77d0a606a1f876065bb5a1cd1a06d022e02927f66c60624fbd5408
+size 1547
