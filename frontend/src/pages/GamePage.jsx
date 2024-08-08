ef39cc3411d6ebd43a396b44c55f5042ae0a4c4b
박수빈 <tns027@naver.com>
2024-08-08 14:01:31 +0900

    게임방 렌더링 완료
    Former-commit-id: 3ea0129c3e2758df7dfef650bd7302f744abdcab

--- frontend/src/pages/GamePage.jsx
+++ frontend/src/pages/GamePage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:402207f7e3659696f736358fcd8d76ab9ab92b37eb823e097a1dbca1f6467422
-size 2478
+oid sha256:521cd48871e1abed96284df07148f02dcc036f4b69dd60eef2067de8e3f640cc
+size 2928
