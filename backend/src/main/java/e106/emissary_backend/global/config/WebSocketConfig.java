7dc39d77dc5ecc744cd37d6708c5bc7d9d8da86c
김강진 <kkj1250@gmail.com>
2024-08-08 22:22:40 +0900

    [fix] fix Merge Conflict
    Former-commit-id: 985986fc5b0e02a9785d551e26f3e4a1af0aff18 [formerly 4b88fb48dc9b961f8876dd517cc2793bed878f03]
    Former-commit-id: 300822b8c19b253e15aa85765bbc3a737532048b

--- backend/src/main/java/e106/emissary_backend/global/config/WebSocketConfig.java
+++ backend/src/main/java/e106/emissary_backend/global/config/WebSocketConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f2ab1b1f699a8faf1c11510f468db9fe5963aa5ea346c89afe2feef4042a5bb
-size 1559
+oid sha256:64dfaed46a79fc9deec2aa7ebd1d206f8318a6aea5bc9ac8b1c9f200b6d9cd10
+size 1513
