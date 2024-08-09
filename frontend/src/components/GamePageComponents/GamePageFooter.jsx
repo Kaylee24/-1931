6fde82d4fd547aba5ec4ecd196b57a20e0a2bb72
김종훈 <boll0918@naver.com>
2024-08-09 10:44:18 +0900

    [feat] GameChat에서 밀정/변절자 구분 기능 추가
    Former-commit-id: 21d030e5a12a1ba1a2604f1c131245ab178fa10a

--- frontend/src/components/GamePageComponents/GamePageFooter.jsx
+++ frontend/src/components/GamePageComponents/GamePageFooter.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c9a1c3ef43d016ba5b099fb82c624085affbb9e56895053d0e2f996469d1a2d
-size 1604
+oid sha256:be7765b7060b18d4c72ffd7f92bec7ced06c627e664c72d5f4a7c0d99d72c223
+size 1663
