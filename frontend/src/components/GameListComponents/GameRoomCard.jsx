8800c9040da76074c9a6fd36eb375fa64fcfcab5
김강진 <kkj1250@gmail.com>
2024-08-10 01:37:20 +0900

    [fix] fix conflict while merging
    Former-commit-id: 8b1c49d390c72862f998f0a67adb78f42e8694a5

--- frontend/src/components/GameListComponents/GameRoomCard.jsx
+++ frontend/src/components/GameListComponents/GameRoomCard.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9272e13f2f93e9c9edc0bcbec92b4a3859158eb212066f4b4dda3845c9afd201
-size 2531
+oid sha256:3d5fad3d71e280088420a0b162d102b4a73fd3944781b2d986c24582acf1c9f3
+size 2453
