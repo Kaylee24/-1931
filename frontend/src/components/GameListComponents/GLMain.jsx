6d7b0becb0d0ec95d0e75bf1cd766ddf86bdf160
박수빈 <tns027@naver.com>
2024-08-10 00:06:01 +0900

    [✨feat] 업적 페이지 CSS 작업 중
    Former-commit-id: 45beae2791ec5d490ac3c9700cf432be2950fa18 [formerly 637b00b3c69ed231ed616b2b318d0706eb2be0b0]
    Former-commit-id: ce9f0fc286f8d1388aaf00b5781358a52ccd8044

--- frontend/src/components/GameListComponents/GLMain.jsx
+++ frontend/src/components/GameListComponents/GLMain.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd3065727458be5653275e396fdc3e35f726de891acd76c5f4ed0903977afc12
-size 1625
+oid sha256:3a5b6161bce157493217fee2aa3aa458d44f337c555043ca0a27af341ac648c3
+size 1607
