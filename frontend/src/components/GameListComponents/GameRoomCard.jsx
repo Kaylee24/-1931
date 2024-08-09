774b582953e05df9837f5171f91ab4b950376892
박수빈 <tns027@naver.com>
2024-08-09 09:16:12 +0900

    [✨feat] 게임 세팅 모달 변경 중
    Former-commit-id: a8e478ed6926b65b0128521ef51b72d02dd46335 [formerly d13a2ab2afece167e4c97d6c82757942db19add9]
    Former-commit-id: 78a88a46a2a1714f18571164e23852922ae943f2

--- frontend/src/components/GameListComponents/GameRoomCard.jsx
+++ frontend/src/components/GameListComponents/GameRoomCard.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d16bdf383f88753d0c09bd525a13a33b573a1f08120ee09d4752431ad2b61bf0
-size 1870
+oid sha256:0d3b08f97612bd7ae40ee0a01b058c7f2e3d5ed8935cf992561ba8bd4c66b01e
+size 1896
--- frontend/src/components/GamePageComponents/GamePageHeader.jsx
+++ frontend/src/components/GamePageComponents/GamePageHeader.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:426c5249e525ee1ef7732f39662b4ccb0b6db8b6ddd8ab722c12e103cc8900f9
-size 2131
+oid sha256:154a6ade14207b81adcb769cd5a45dee058990939a7ccfea6672f7995e10960a
+size 2132
