12ba804fb276f1b32b08ff512e4e2f63481cfb37
이현규 <gusrb2776@naver.com>
2024-08-09 17:37:46 +0900

    [fix] fix Enter Message
    Former-commit-id: fe469edada8afe9b5051cedf4d14b973d6c67cd5

--- backend/src/main/java/e106/emissary_backend/domain/room/service/RoomService.java
+++ backend/src/main/java/e106/emissary_backend/domain/room/service/RoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6178f8181fbce9794f93a8570b3bed3bc9ddb8841f05638c7b16dce4f74c328b
-size 21418
+oid sha256:2b835d61fd5d007b110d2aa2519fc2d935975975c65572ed2c27791475b29cff
+size 21123
