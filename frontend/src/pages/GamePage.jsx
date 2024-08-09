52d754d32e565e577ddfa25595d09c4678b36af8
박수빈 <tns027@naver.com>
2024-08-09 16:20:55 +0900

    [✨feat]  리스트에서 게임 페이지 입장 가능 / 게임 설정 수정 중
    Former-commit-id: c13f3c2913fdf0621daceefe7cc3c480768280ac

--- frontend/src/pages/GamePage.jsx
+++ frontend/src/pages/GamePage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a16bb3e2bb3991389076af5818f79007b899a16c2d3f2edf88a8df7c9249fc5
-size 10429
+oid sha256:32c17bab225d14b9a7cb431e9f608d4b44887160118cf28ad1ba529e3c6c1e2b
+size 10427
