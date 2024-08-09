247becc8593f14b4bb7c77c6435f285cbda22db9
이현규 <gusrb2776@naver.com>
2024-08-09 10:05:57 +0900

    [feat] implements JWT Authentication in WebSocket
    Former-commit-id: 1a7606a6aaa9bdee79e88b8d242a6cbcf6266922

--- backend/src/main/java/e106/emissary_backend/global/config/WebSocketConfig.java
+++ backend/src/main/java/e106/emissary_backend/global/config/WebSocketConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b3a7011d9e4cab2a5f0cb3d5ff04bafc4bb14bbb96196ac5eb86150153036f8
-size 1507
+oid sha256:aa1131407065bb0573cc81302c455f167471e63418cc2e71a8397e0a1bc90037
+size 1687
