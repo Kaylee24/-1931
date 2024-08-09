e43f026a6b3df683736843cd10c2a4c576ab8b46
김송희 <kkbuh2@gmail.com>
2024-08-09 22:03:03 +0900

    [feat]isLoggedIn 여부에 따른 GameListPage 이동 차단 및 MainPage CSS | 버튼 클릭 안됨 Error 발생
    Former-commit-id: da8cf7ccb8586582bbf1e2fcf41876fe05bad880

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c0d11362e485f626bd7fd34f34cc8696a61bad2d5d018756aafd10a0f483493
-size 644
+oid sha256:d8f4cb8a18e2fd4989306e2d4c7ef78a64144f4d226d0423ddd40e2ce4174b14
+size 2181
