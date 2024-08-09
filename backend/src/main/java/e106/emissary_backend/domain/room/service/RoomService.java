d561df9ca5eebea917475f88640c175c80b7fc56
이현규 <gusrb2776@naver.com>
2024-08-09 16:24:16 +0900

    [feat] add enter message function
    Former-commit-id: 608c194b3d2773aeb4034691bc906f591db60253

--- backend/src/main/java/e106/emissary_backend/domain/room/service/RoomService.java
+++ backend/src/main/java/e106/emissary_backend/domain/room/service/RoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ae205910a2de9d7f71c7adbc4834facd82cdb43132dd1bfa08b8b1f4b6213ea
-size 18592
+oid sha256:2ae65d377e95776de97dd6119a6f0528ac780f7ae9b63671b4cb424a6adaf741
+size 21348
