e4780f2b90ee5ab637347577e1fcc767786db9c5
박수빈 <tns027@naver.com>
2024-08-09 12:10:56 +0900

    [✨✨feat] 게임방 게임 설정 모달 CSS
    Former-commit-id: 34344ce46253dd16849100c3409810703ca10c07

--- frontend/src/pages/GamePage.jsx
+++ frontend/src/pages/GamePage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d9f847f9d306378736a0e1cc5b264e4d8c99e29d20a124e6a17ace12dcfc5ad
-size 9861
+oid sha256:3a16bb3e2bb3991389076af5818f79007b899a16c2d3f2edf88a8df7c9249fc5
+size 10429
