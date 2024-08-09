089b385b70235cf2365668423ae915fa77204879
이현규 <gusrb2776@naver.com>
2024-08-09 20:44:31 +0900

    [fix] delete already User in room when enterRoom
    Former-commit-id: 74bcf2198abb36760cb270047854a891aae049bd

--- backend/src/main/java/e106/emissary_backend/domain/userInRoom/repository/UserInRoomRepository.java
+++ backend/src/main/java/e106/emissary_backend/domain/userInRoom/repository/UserInRoomRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69209fbe8f6228a796bdf5bf3c35c78c71f940a270fba5060d860a0339af177b
-size 1108
+oid sha256:935f1a28131ab9d0523e2915ba3890abf6b9b2487e58d353a770b2cf8ce39288
+size 1150
