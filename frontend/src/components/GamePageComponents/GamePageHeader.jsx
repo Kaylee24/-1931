f39737a540bb46b46a2f465cff09a51ef71cf639
kkj1250 <kkj1250@gmail.com>
2024-08-12 02:01:12 +0900

    [fix] leave session done
    Former-commit-id: a2b5dc311bb0618759225d4502961ebc1d746ad8 [formerly 0b58204aaf6b2aa77d187f1e9ff6f23fe25e068a]
    Former-commit-id: 8668965bb97c998ab44d4c84d6eba3496a5abcd9

--- frontend/src/components/GamePageComponents/GamePageHeader.jsx
+++ frontend/src/components/GamePageComponents/GamePageHeader.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6248dbdb3727a4cee070697bc329a80705dd704d49572eb28d3ed20b9cd2734c
-size 6188
+oid sha256:e1813d033050b23f2e26011aea62128c0726185b5fab9a79d47b388af194f02c
+size 3592
--- frontend/src/pages/GamePage.jsx
+++ frontend/src/pages/GamePage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52bd2a661236d4f61fb60bac2d04e69da8495a19eb3d09dc7fc6466948fe6c92
-size 16892
+oid sha256:281aab11af1fa4c25ab70b69cd6be00fe5b39d4f5d1fda31f9973f0ad109cde5
+size 16797
