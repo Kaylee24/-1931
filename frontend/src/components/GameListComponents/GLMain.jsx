fc7870170fda6290c79d6117031bbcea7d401355
김송희 <kkbuh2@gmail.com>
2024-08-11 14:24:46 +0900

    [fix] CSS 작업 꼬임 이슈 정리
    Former-commit-id: 9b8ccb11d9da03f400e660fb04042a0de6645789

--- frontend/src/components/GameListComponents/GLMain.jsx
+++ frontend/src/components/GameListComponents/GLMain.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08bd45604b8fb791bbac87a07573cbb5f9a9aebaf34689fa75f59e5e787f1cbe
-size 2069
+oid sha256:856ea1fbd1f27f4ea22a679a18480e4ac6d4584d9c2c6094de9e20781b10c70a
+size 2051
