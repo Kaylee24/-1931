f0321fd0c2395408754a255bb389a2211f66a917
박수빈 <tns027@naver.com>
2024-08-11 16:33:48 +0900

    게임 로직 수정 중
    Former-commit-id: 58bc167a8af285a38fb341023ca7b2440a53730d

--- frontend/src/components/GameListComponents/GameRoomCard.jsx
+++ frontend/src/components/GameListComponents/GameRoomCard.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d5fad3d71e280088420a0b162d102b4a73fd3944781b2d986c24582acf1c9f3
-size 2453
+oid sha256:66e3fe93951e93870e000d29174fcae498e4ceb333209952f52d8688efb43a7d
+size 2536
--- frontend/src/pages/GamePage.jsx
+++ frontend/src/pages/GamePage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48e6518480f4ec06f5bb3688eb63d495ccd4c088b96a4c0a31feb1c7e32b981f
-size 16861
+oid sha256:42bfd3b5357ff82858d41440763481b8c56bff9ad2f48334ea256bfe39ccd5b5
+size 16925
