38f408725787fa57e84866dd0a7773c31f0f2262
김송희 <kkbuh2@gmail.com>
2024-08-11 17:30:12 +0900

    [feat]GLHeader CSS 수정
    Former-commit-id: 6a8e4ba1cc65e4e048598af08faec655f23de88e

--- frontend/src/components/GameListComponents/GLHeader.jsx
+++ frontend/src/components/GameListComponents/GLHeader.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d63be3bb7281798219331c0fa7be1ea3cc0afd21e969cf778ade29fb26013a3a
-size 1545
+oid sha256:3a3ce1c0f713bd69f8993426d1824e17e67e7594d7ab3303db126fff3f5e4710
+size 3436
