fe8a93478c623396f82da2c364f95197e591b9f4
박수빈 <tns027@naver.com>
2024-08-11 23:03:42 +0900

    GamePage, GamePageHeader, GaneReadyStartBtn, GamePageFooter, GamePageHeader 코드 정리
    Former-commit-id: cce436f1cf6c16de470777b57672df971259059d

--- frontend/src/components/GamePageComponents/GamePageFooter.jsx
+++ frontend/src/components/GamePageComponents/GamePageFooter.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40fd53fedd95c49005c5ffc23cd1b4442e6526747f8742084dbb5f3c13cd8c58
-size 1701
+oid sha256:17019d26448ba3c11fc83302e71018c32e851967978cbe088fce36574471adcd
+size 1725
