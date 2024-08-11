29cc4a7fbe0452b7075b0787f3032073e1c1783c
김종훈 <boll0918@naver.com>
2024-08-11 22:15:01 +0900

    [feat] 방목록 필터링 조회 기능 API 추가
    Former-commit-id: 71802cafdf593a744390f23543e39d8809b5a021

--- backend/src/main/java/e106/emissary_backend/domain/room/service/RoomService.java
+++ backend/src/main/java/e106/emissary_backend/domain/room/service/RoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3fced06bc40fa5dc16c7977d5af1672a750a0c89f6a2919fb731c70494eaa12
-size 21193
+oid sha256:082c8ca3bf8c8ce3ebf4780a4128c1207396eda30fbece35df8d3798dde7f994
+size 23073
