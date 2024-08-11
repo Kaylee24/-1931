2f46a2b8966c7b5b15a4dd7dd0dcea034e09fd75
김강진 <kkj1250@gmail.com>
2024-08-11 15:04:32 +0900

    [fix] save vidu token when join room
    Former-commit-id: ec01af2a484ad863f4e3b7872afc96a63c53d9c7

--- backend/src/main/java/e106/emissary_backend/domain/room/service/RoomService.java
+++ backend/src/main/java/e106/emissary_backend/domain/room/service/RoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:04c915c1ca201160f301f4894c30e04152cc23288169a46dac15a0535af54b53
-size 22108
+oid sha256:8fc7c94c5989bb43d780dc8f008b89f17b4c8dde4106783cbf54d6770ab39f50
+size 22146
