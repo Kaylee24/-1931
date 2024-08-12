b1d67e2338f972b7513a02b99137cccf3907952c
김송희 <kkbuh2@gmail.com>
2024-08-12 16:38:04 +0900

    [feat] GameCard font 누락 및 font url 주소 누락 건 수정
    Former-commit-id: 20e58638548c543445311b6477ee1a6e0daa506b [formerly 1dbe6b0333b3244785070e04fb5bee6d76b04d1f]
    Former-commit-id: 05d010f929150f37d821bb86692e6756a86e651d

--- frontend/src/components/GameListComponents/GameRoomCard.jsx
+++ frontend/src/components/GameListComponents/GameRoomCard.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6089d9a22d4d82ef998ae2a3a2506fce09a57e6239229f7f50264b7210fe7e21
-size 2379
+oid sha256:ae0c149c36bf3ef8853915aaaf9b02e294befd8083e795ab17099a94272f8aa2
+size 2471
