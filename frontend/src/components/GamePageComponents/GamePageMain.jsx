129719a4fbadb1de5335c795f5a958490bdd06e7
박수빈 <tns027@naver.com>
2024-08-12 23:51:12 +0900

    [[✨ㄹfeat] 직업 카드 생성
    Former-commit-id: 244f54c171d233de64f4d68af437283bea0d1f18 [formerly eeda3fd0495635f6ddacc0d58486b2ca3529ba36]
    Former-commit-id: 22357243e07e2efb2b8a2632b53df3c4c09562c2

--- frontend/src/components/GamePageComponents/GamePageMain.jsx
+++ frontend/src/components/GamePageComponents/GamePageMain.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2c62e12fa33e029bdbddbe99902e021a3c73b7cd686a6117c10eff3c2536bf6
-size 15755
+oid sha256:f2cfac098212c12c4bd9a2d7c33677d5b7befcb92e821d4a5e4690c6209f1f71
+size 16871
