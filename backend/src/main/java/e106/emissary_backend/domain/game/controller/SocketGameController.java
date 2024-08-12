976df434cb8e4f7f37efdd637d6eab758ed8a375
이현규 <gusrb2776@naver.com>
2024-08-12 17:22:00 +0900

    [feat] add start game logging
    Former-commit-id: 5b7bf8ee3ec6cbeb8bb39767012f86b30c22281c [formerly b1206a3a3485e2cd96fc26a69236798a857946db]
    Former-commit-id: 362aa3cb0945e541a171c3c1d270c4f78b558e7a

--- backend/src/main/java/e106/emissary_backend/domain/game/controller/SocketGameController.java
+++ backend/src/main/java/e106/emissary_backend/domain/game/controller/SocketGameController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a6ee3eccc5056021eb686998f77e2328e0dd1acf5d15e8cbc4a9f60cae73eee
-size 4599
+oid sha256:fdfa0df64b2362a77bbcbd265841e3c362f743fcc69e641eb88e4d711ac75721
+size 4633
