966a91322b5de58594d578c1f08b45f4aa3ec973
김송희 <kkbuh2@gmail.com>
2024-08-13 05:51:26 +0900

    [feat] GamePageMain, Monitor CSS
    Former-commit-id: b57294d04be9eee2b29dde2a30e1c0e603ffae40 [formerly 434f7242410260c75bd27382290f31e697199da9]
    Former-commit-id: a4386da57cb4aad5ab2b19977299ba46b481a983

--- frontend/src/components/GamePageComponents/GamePageMain.jsx
+++ frontend/src/components/GamePageComponents/GamePageMain.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77f832c6fc51851b8248486ea06d08a2868bd4c076f9a896a446835bbb1ad3ff
-size 16635
+oid sha256:a8df480b75f10fec7bb9a66a4526622a8b1c2944ac0d06b29d95df56f46b6d18
+size 18237
