610eb6da8eb90b46083b185903c91fc7b6220a90
이현규 <gusrb2776@naver.com>
2024-08-12 09:26:31 +0900

    [fix] fix RoomDetailUserDTO
    Former-commit-id: 64f582b0ef0bede5a86e514a7e0c202c75c2d0f1

--- backend/src/main/java/e106/emissary_backend/domain/user/dto/RoomDetailUserDto.java
+++ backend/src/main/java/e106/emissary_backend/domain/user/dto/RoomDetailUserDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff2f5d5497216fe0aa59f24ada062aa0b26ca47102d45061553e642616706749
-size 1159
+oid sha256:212bd7daa9de3fd7fc0b31b21e9ccb5db7d6cf2853f16249acc60fd445d29329
+size 1113
