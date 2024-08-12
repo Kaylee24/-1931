031889454b9c959171ac429a2424e4f86447efad
김송희 <kkbuh2@gmail.com>
2024-08-12 17:52:34 +0900

    [feat] GamePageMain CSS 작업 중
    Former-commit-id: 2041d6441877bf11cc8d62c40c2c88f0f86017da

--- frontend/src/components/GamePageComponents/GamePageMain.jsx
+++ frontend/src/components/GamePageComponents/GamePageMain.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ee41ce04d63c5a098703a632653f0907a75c1f85ac361d1a72f18b55fb5115a
-size 16051
+oid sha256:77f832c6fc51851b8248486ea06d08a2868bd4c076f9a896a446835bbb1ad3ff
+size 16635
