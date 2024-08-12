ad2e87bc48aaf3bf1e44fdfa4e73eecb3c2a65dd
김강진 <kkj1250@gmail.com>
2024-08-12 16:59:15 +0900

    [fix] remove <p> @ gamepage main.jsx
    Former-commit-id: 6091cef9cd61ec072fca9366403f2b63100b9cf0 [formerly 9fc1ce93f8a62d47a3706083669e23ef25a8f8e5]
    Former-commit-id: d0fed0ad25f8e583ca376dd2a671f3807dc1da19

--- frontend/src/pages/GamePage.jsx
+++ frontend/src/pages/GamePage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:438c87209b7b2f2731d8a785fc528c826b3f01edd7a72e92d9388407cb9dd8e9
-size 15217
+oid sha256:b0d1e4a9fea8961c3e868b2f090b5c799f5d4058b5d2c64381132cf7cf5363c9
+size 10655
