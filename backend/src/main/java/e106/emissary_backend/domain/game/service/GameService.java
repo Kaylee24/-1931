a3db6c176474a7f57d66b81d426f02c2019ca36d
이현규 <gusrb2776@naver.com>
2024-08-12 16:29:22 +0900

    [fix] fix ready btn
    Former-commit-id: 9dbf3046f41524d3df464c44184db6ef59e26154

--- backend/src/main/java/e106/emissary_backend/domain/game/service/GameService.java
+++ backend/src/main/java/e106/emissary_backend/domain/game/service/GameService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:970604bd04dbcbd00c7d7cf87008288f51dcda6bd6abaa70cad279d859f6b38d
-size 14985
+oid sha256:72243ee3efeb4aeb0f98f63ac9f98e12d48f950bb27fb03481dc86150ef7c15b
+size 15391
