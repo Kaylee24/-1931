a81c3b818f87bea80a117afcd48767f8bdf921b0
김강진 <kkj1250@gmail.com>
2024-08-12 15:52:13 +0900

    [fix] Websocket Button Fix
    Former-commit-id: 31655770ff2116f65f626a04aed3f06e17e7aa09

--- frontend/src/pages/GamePage.jsx
+++ frontend/src/pages/GamePage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a80c413103a8234dd2710cd89446ba89e404fe5060d7ae7845d74cc892875f9b
-size 14706
+oid sha256:438c87209b7b2f2731d8a785fc528c826b3f01edd7a72e92d9388407cb9dd8e9
+size 15217
