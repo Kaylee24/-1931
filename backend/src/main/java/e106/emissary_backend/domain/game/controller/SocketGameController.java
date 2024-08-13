47cae2c3a3ccb4fcd6732240e9cb229ce58f3164
이현규 <gusrb2776@naver.com>
2024-08-13 14:32:55 +0900

    [fix] change Connect Message
    Former-commit-id: 39c32515f4248a2b553cd6f7261991df5afdb443

--- backend/src/main/java/e106/emissary_backend/domain/game/controller/SocketGameController.java
+++ backend/src/main/java/e106/emissary_backend/domain/game/controller/SocketGameController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fdfa0df64b2362a77bbcbd265841e3c362f743fcc69e641eb88e4d711ac75721
-size 4633
+oid sha256:a3574c8dbb0dd690e109fbe456ffa1e3038959dc5d83cb84e740252ba34f231d
+size 4688
