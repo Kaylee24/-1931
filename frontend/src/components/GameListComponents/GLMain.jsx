bd3f81cb39ace2ab43755666bc88ee3043c966af
김송희 <kkbuh2@gmail.com>
2024-08-13 23:09:39 +0900

    [feat] GameListPage filter, search 구현 완성
    Former-commit-id: 87ae4a178aa5a7520decdd9108af9420f367a450

--- frontend/src/components/GameListComponents/GLMain.jsx
+++ frontend/src/components/GameListComponents/GLMain.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a20ba9923dcd5230037d011703d309d70a12b7c275fbd6754aae613f61cd0003
-size 3950
+oid sha256:03de4b7332b5735e45bffa75b884fad2f64a6eb3c95e6ee086928ae76129f297
+size 2777
