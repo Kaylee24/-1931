4f38ba307c41dde23fa37aef5c1ad04588592135
박수빈 <tns027@naver.com>
2024-08-13 10:22:59 +0900

    직업 모달 코드 수정
    Former-commit-id: c4ee32822fd042d6f2aab5401500cd22ad7af34c [formerly 24bcdd93bcaaa09df5681df71494e440564334db]
    Former-commit-id: e6f3f531575b1d14e4109c8caa0f87bf185b0294

--- frontend/src/components/GamePageComponents/GamePageMain.jsx
+++ frontend/src/components/GamePageComponents/GamePageMain.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68f554e54fdff7fe091a7e13b523b0409a6eaba6262a9911389e774beb7e47bb
-size 16871
+oid sha256:5442504a187bd67a038a9127eb4032f631b7b1c905fed3316ec41985f24c6444
+size 17375
