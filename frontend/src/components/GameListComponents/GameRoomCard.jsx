d77e00dc34e4975e1b1f2111d350d2930c2d303f
김송희 <kkbuh2@gmail.com>
2024-08-13 15:51:43 +0900

    [feat] 입장하려는 방이 풀방이면 입장 불가 로직 추가
    Former-commit-id: 1d81a3b0933faa13b0b82bc71d33251dbf5e32e4 [formerly 21969acbe7ce18f22c96ecc91106c602905b4369]
    Former-commit-id: 5ccf09972d8273f1a9c61e4d81755623fbe1ea33

--- frontend/src/components/GameListComponents/GameRoomCard.jsx
+++ frontend/src/components/GameListComponents/GameRoomCard.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6089d9a22d4d82ef998ae2a3a2506fce09a57e6239229f7f50264b7210fe7e21
-size 2379
+oid sha256:fcf2f1d744bd4d053a6e008336b7ca93bf113ccf81688b1f76c4d688965cf66c
+size 2030
