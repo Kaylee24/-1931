2b2a72ee1c72c204db07fa17170c3f1964bb4644
박수빈 <tns027@naver.com>
2024-08-13 22:29:47 +0900

    [fix] 밀정 밤활동 가능
    Former-commit-id: e2d655beb1d1f30864184692780c9f5ab76c47f1 [formerly 2ee5a6d06aabe32a5da44996d1d338cde2ceb4d4]
    Former-commit-id: 702d837aee99a709cf2c8c7f078a3127680afa10

--- frontend/src/components/GamePageComponents/GamePageMain.jsx
+++ frontend/src/components/GamePageComponents/GamePageMain.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:315376def41bf74f18bcb04f1f77e1a5aea3a8483b396caa81901c53a17e6e0d
-size 19413
+oid sha256:97e9b21c26e0cb139ce6595f9fbe2d89419eb9e12fe3da02fd6f8d15750518cb
+size 20082
