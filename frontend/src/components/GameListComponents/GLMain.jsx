9d3273c23f996f654415a764ddfd553159d70bff
김송희 <kkbuh2@gmail.com>
2024-08-13 16:17:48 +0900

    [fix] GLMain font 없어짐 수정
    Former-commit-id: 67bda21df5ca85641988914339ead54e2731d0a9

--- frontend/src/components/GameListComponents/GLMain.jsx
+++ frontend/src/components/GameListComponents/GLMain.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc6ca3d6fa4103021350460db4f6cf59c65f3b5ad464b3e9da5eec9bbe8fe387
-size 1730
+oid sha256:c46014e1361e3a432d7ca1b826ca00b52f326d02ac9c7b855ce06bdb32c65c9d
+size 1773
