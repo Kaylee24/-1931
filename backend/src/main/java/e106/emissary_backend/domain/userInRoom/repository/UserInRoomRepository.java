60d17242de0d40a8b47f987a2cac3a5b0e881911
이현규 <gusrb2776@naver.com>
2024-08-13 22:40:04 +0900

    [fix] fix leaveRoom and kick user
    Former-commit-id: f740be74636efe5fef7904a25b5b03a1a62f7f92

--- backend/src/main/java/e106/emissary_backend/domain/userInRoom/repository/UserInRoomRepository.java
+++ backend/src/main/java/e106/emissary_backend/domain/userInRoom/repository/UserInRoomRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:935f1a28131ab9d0523e2915ba3890abf6b9b2487e58d353a770b2cf8ce39288
+oid sha256:97c70acc78e1b761f62a3f07796d221f95cebdd9475503d0d7c252455f2e1ebb
 size 1150
