c5fb3efbd736bf3c8476acd51b497eb0f9070669
김송희 <kkbuh2@gmail.com>
2024-08-13 20:59:29 +0900

    [feat] GameRoomCard 컴포넌트에 극비 임무 떠요!!!
    Former-commit-id: 2d9f9ac811b58bc1d69be6192e41000974b53f4f [formerly 61394ceda110818b74094678719378fb08f0ef02]
    Former-commit-id: 0a3706dece3ddaa4a5d7e19d99f619a8c1293d97

--- frontend/src/components/GameListComponents/GLMain.jsx
+++ frontend/src/components/GameListComponents/GLMain.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:795b744c6fc058ffb7be34dd0c8d0ecd4ce03b5b5a7b88b680d281e58b455c32
-size 3746
+oid sha256:ff7b9ec818045431d7102dadd46f8996d3bc38c60c12c0e56cc58a27094c81ab
+size 3789
