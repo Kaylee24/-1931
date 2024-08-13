f9943cb171ddbe97f3e256bc3bedf3ff5f8b529b
박수빈 <tns027@naver.com>
2024-08-13 20:12:23 +0900

    게임시작 확인 중
    Former-commit-id: 18a04e389e4574b2ee414f83bd7a2cef8bee7fdf

--- frontend/src/components/GamePageComponents/GamePageMain.jsx
+++ frontend/src/components/GamePageComponents/GamePageMain.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4820fb007bcca18222885b47697b4b7dd99a5b0807849b7640c97ab0bace2f56
-size 18896
+oid sha256:6d25260f2f4cf32de2232a4f9bbb1dff8496eb86f57a00939a0066ecb6a6e963
+size 19093
