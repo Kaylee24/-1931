6177a78fabe9b773f6461aadd27f7673467a16ee
김강진 <kkj1250@gmail.com>
2024-08-13 11:49:21 +0900

    [fix] fix merge conflict occuered by human error
    Former-commit-id: 85de992d2b21e087d34df1b09fed09fee89f0ae0

--- frontend/src/components/GamePageComponents/GamePageHeader.jsx
+++ frontend/src/components/GamePageComponents/GamePageHeader.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad6e292449f5b537e0e630c6953a8d75031dd37bca22714c4227d334bd67579d
-size 3393
+oid sha256:c20ebc39b298fd70ae32093aa51eca3eb954bcd2c491b5561b221222ead43ae7
+size 3538
