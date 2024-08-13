ff6b47305fb1b809e2d8381fe938cbc7c001300b
박수빈 <tns027@naver.com>
2024-08-13 15:29:11 +0900

    업적 페이지 CSS 완성
    Former-commit-id: bc7d22af1d21baab5df593cdca6ef314ae2a909e

--- frontend/src/modals/JobModal.jsx
+++ frontend/src/modals/JobModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae651a2ca56d7b4e036206aa52851c0956156446f190583c0c97a86efa8d5b95
-size 3163
+oid sha256:26c81e035f417e3d9a4053b6a8a335ce58f2b8d9476ed26e866f93b4dcc16e0b
+size 3384
