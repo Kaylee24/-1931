704e3ba4d808f79b3431170001b0776ebd455147
이현규 <gusrb2776@naver.com>
2024-08-13 14:47:16 +0900

    [feat] add log ReadyCompleteSubscribe
    Former-commit-id: eb9cf8a4c11925460d2ee0dc064b32ec5903ff2a

--- backend/src/main/java/e106/emissary_backend/domain/game/service/subscriber/ReadyCompleteSubscriber.java
+++ backend/src/main/java/e106/emissary_backend/domain/game/service/subscriber/ReadyCompleteSubscriber.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c14ba1356a13e4a5e3cc81e9923ff91a5aa7c10f1704cda565e3a139f53a152
-size 1160
+oid sha256:80595a13c324c50f00962e46d416b8344abcb9fc83426efae11574b4d0656d4c
+size 1154
