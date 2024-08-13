70af26027d6150eb55181be17257aa5d17af28bc
김송희 <kkbuh2@gmail.com>
2024-08-13 09:02:42 +0900

    [feat] GamePage 관련 CSS 작업중
    Former-commit-id: 7a382cd2b43146662728aa7fb42b8563fdaf301e

--- frontend/src/modals/GameSettingsModal.jsx
+++ frontend/src/modals/GameSettingsModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5c8d493de13cba662f2b860e91febd23f9923b494d7109a77455a8fa206ce5b
-size 5945
+oid sha256:02b457d8970fd6d091850b82e0b8537c18753c103b32458eac6119fd33dd5ace
+size 6420
