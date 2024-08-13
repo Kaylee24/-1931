3f39a8a0b2c7fe6db2180800239a506ee0516ee2
김송희 <kkbuh2@gmail.com>
2024-08-13 21:19:21 +0900

    [feat]GameListPage filter API 연결 완료
    Former-commit-id: a30873d3816333560f1ef367edb7a4d1ce72e558 [formerly d950e59f5cbb0f6184d1bb321b80e9c3a61b25a4]
    Former-commit-id: f22de92c04de25a130632e1721013f53fda1d421

--- frontend/src/components/GameListComponents/GLMain.jsx
+++ frontend/src/components/GameListComponents/GLMain.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff7b9ec818045431d7102dadd46f8996d3bc38c60c12c0e56cc58a27094c81ab
-size 3789
+oid sha256:a20ba9923dcd5230037d011703d309d70a12b7c275fbd6754aae613f61cd0003
+size 3950
