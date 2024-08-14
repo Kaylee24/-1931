36fc0220584f551332233bcf6e58f55e7c37b485
박수빈 <tns027@naver.com>
2024-08-14 14:50:06 +0900

    경찰 모달 확인 중
    Former-commit-id: 5958b8e952834e408dedab7d1c0d48805f6c5305 [formerly 5f32982628dcb4a11ac290c2e8dcbc8f48866c6e]
    Former-commit-id: 997527f17a38583e39703555383a68662984b7d5

--- frontend/src/components/GamePageComponents/GamePageMain.jsx
+++ frontend/src/components/GamePageComponents/GamePageMain.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d3c5032d5ce411aff6983751418ab6e5bd386c4168354d22353ca4983281146
-size 21665
+oid sha256:fadb0ec5a3ab032d5557d21a75cda6e815530d095c95fcb8c8ea3cd95af87d62
+size 21632
