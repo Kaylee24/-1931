387e9f3d1939a8fd00ca8019b27120e4b5f200e3
김강진 <kkj1250@gmail.com>
2024-08-14 16:34:32 +0900

    [fix] unclosed bracket
    Former-commit-id: 629c816590a4d0fbe8b0fad55bbe148a72082c1c

--- frontend/src/components/GamePageComponents/GamePageMain.jsx
+++ frontend/src/components/GamePageComponents/GamePageMain.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f3ec05e7cde1036e53060b8cae43dfcd1c32cb3fd12bd76e3d6f1cdbeacadd1a
-size 22367
+oid sha256:8b825409c62d7c706368a97c630012545f14d9e92879f22d05c1cd0d8701e9eb
+size 22374
