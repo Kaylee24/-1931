4968f2be7a6ab8532df23281474c21b6afd260f6
김강진 <kkj1250@gmail.com>
2024-08-14 21:52:52 +0900

    [fix] fix video
    Former-commit-id: 40a57d492e051a8825144010e9e52e5f09819049

--- frontend/src/components/GamePageComponents/GamePageMain.jsx
+++ frontend/src/components/GamePageComponents/GamePageMain.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c2e8984e57d89cd6b251e19f7fe6157d21cf1f69944c89c1c0fa99ed579a359
-size 22386
+oid sha256:5e9dbad7e98a18b7e9bc8a3351f7d33dc690172b1cee7a7dee065ebee98b5501
+size 22290
