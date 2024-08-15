497a22774a8388fffbc3720a39049f5848154957
kkj1250 <kkj1250@gmail.com>
2024-08-15 22:40:13 +0900

    [fix] game room broken bug fix
    Former-commit-id: ec26eefa798d4cfb8d7c67f64d1507780a02c349

--- frontend/src/components/GamePageComponents/GamePageMain.jsx
+++ frontend/src/components/GamePageComponents/GamePageMain.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:885ad7584b136cf3d3530fb6a5c1d0bfd2ee98da777c1e1ac46dc3cd5923b9df
-size 26277
+oid sha256:de321e1b3307a8f7ad359ec07be9c09754be73b3555e59f612555885ee83c947
+size 26437
